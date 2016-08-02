f467cb6672083d0f4595ea4a0c4919e26f7e02fd
Florin Broasca <florin.broasca@gmail.com>
2016-08-02 16:13:15 +0300

    [BUMP] version 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tropopause',
-    version='0.3',
+    version='0.4',
     description='Extra utilities for troposphere',
     classifiers=[
         'Development Status :: 3 - Alpha',
