b07433909c9498c5628b3bf2b0f27ef565ee543c
Florin Broasca <fbroasca@adobe.com>
2017-03-08 11:05:28 +0200

    [BUMP] version 0.9.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tropopause',
-    version='0.8',
+    version='0.9.0',
     description='Extra utilities for troposphere',
     classifiers=[
         'Development Status :: 3 - Alpha',
