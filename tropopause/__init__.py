27c3346dcdbf739e2621c1add110fd4282f80afa
Florin Broasca <fbroasca@adobe.com>
2017-03-10 15:59:34 +0200

    Update regions DNS suffixes

--- tropopause/__init__.py
+++ tropopause/__init__.py
@@ -9,10 +9,13 @@ from troposphere import Template as TropoTemplate
 
 DNS_SUFFIXES = {
     'us-east-1': 'ec2.internal',
+    'us-east-2': 'us-east-2.compute.internal',
     'us-west-1': 'us-west-1.compute.internal',
     'us-west-2': 'us-west-2.compute.internal',
+    'ca-central-1': 'ca-central-1.compute.internal',
     'eu-west-1': 'eu-west-1.compute.internal',
     'eu-central-1': 'eu-central-1.compute.internal',
+    'eu-west-2': 'eu-west-2.compute.internal',
     'ap-northeast-1': 'ap-northeast-1.compute.internal',
     'ap-northeast-2': 'ap-northeast-2.compute.internal',
     'ap-southeast-1': 'ap-southeast-1.compute.internal',
