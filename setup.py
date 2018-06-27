7e55b66a457b57f7cf31afd0865d2f050e329340
Laura Ureche <ureche@adobe.com>
2018-06-27 16:17:19 +0300

    Added support for c5 instances

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tropopause',
-    version='0.9.2',
+    version='0.9.3',
     description='Extra utilities for troposphere',
     classifiers=[
         'Development Status :: 3 - Alpha',
